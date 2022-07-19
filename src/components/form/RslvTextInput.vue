b5b4272f27ff64134732ef99bb248118ccd1380d
Alexandre Vinet <alexandre.vinet@laresolution.ca>
2022-07-19 15:47:33 -0400

    add vee-validate composition form example

---
+++ src/components/form/RslvTextInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useField } from 'vee-validate'
+import { defineProps, toRef } from 'vue'
+
+const props = defineProps(['label', 'placeholder', 'disabled', 'name'])
+
+const { value, errorMessage } = useField(toRef(props, 'name'));
+
+
+</script>
+<template>
+    <v-text-field v-model="value"
+        :label="label"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        :error-messages="errorMessage"
+        type="text"
+        clearable></v-text-field>
+</template>
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <HelloWorld />
+  <div>
+    <RslvTextInput name="siteName" label="Site Name"></RslvTextInput>
+    <RslvTextInput name="fullName" label="Full Name"></RslvTextInput>
+    <RslvTextInput name="email" label="Email"></RslvTextInput>
+    <RslvTextInput name="phone" label="Phone"></RslvTextInput>
+    <RslvTextInput name="favoriteNinjaTurtle" label="Favorite Ninja Turtle"></RslvTextInput>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { useForm } from 'vee-validate'
+import RslvTextInput from '../components/form/RslvTextInput.vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
+const simpleSchema = {
+  siteName(value) {
+    return value ? true : 'The site name field is required'
   },
-});
+  fullName(value) {
+    return value ? true : 'The full name field is required'
+  },
+  email(value) {
+    return value ? true : 'The email field is required'
+  },
+  phone(value) {
+    return value ? true : 'The phone field is required'
+  },
+  favoriteNinjaTurtle(value) {
+    if (value !== 'Donatello') {
+      return 'Your favorite turtle is not Donatello'
+    } else {
+      return true
+    }
+  }
+}
+useForm({
+  validationSchema: simpleSchema
+})
+
+// const submit = () => {
+//   validate().then(result => {
+//     alert(JSON.stringify(result))
+//     console.log(values.value)
+//   }).catch(e => {
+//     alert(e.message)
+//   })
+// }
 </script>
