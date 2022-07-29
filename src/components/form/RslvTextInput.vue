9d8a0366cbcdc45651da245ffc18d62cc94bbbfb
Alexandre Vinet <alexandre.vinet@laresolution.ca>
2022-07-29 15:16:50 -0400

    adds tailwindcss

--- src/components/form/RslvTextInput.vue
+++ src/components/form/RslvTextInput.vue
@@ -1,17 +1,30 @@
 <script setup>
 import { useField } from 'vee-validate'
-import { toRef } from 'vue'
+import { toRef, computed } from 'vue'
 
 const props = defineProps(['label', 'placeholder', 'disabled', 'name'])
 
 const { value, errorMessage } = useField(toRef(props, 'name'));
 
+const errorClasses = computed(() => {
+    return {
+    'border-red-800': !!errorMessage.value,
+    'border-b-2': !!errorMessage.value
+    }
+})
 
 </script>
 <template>
-    <h4>{{label}}</h4>
-    <input type="text" v-model="value" :placeholder="placeholder" :disabled="disabled" />
-    <p v-show="errorMessage">{{errorMessage}}</p>
+    <div class="p-1 flex flex-col justify-between border-2 bg-zinc-100 h-18 relative">
+        <input class="bg-zinc-100 border-b border-zinc-800 focus-within:outline-none leading-10 align-bottom" type="text" v-model="value" :placeholder="placeholder" :disabled="disabled" :class="errorClasses"/>
+        <h4 class="order-first bg-transparent h-4 z-10 absolute bottom-8">{{label}}</h4>
+        <p v-show="errorMessage" class="text-sm text-red-800 h-4">{{errorMessage}}</p>
+    </div>
 </template>
 <style>
+input:focus-within + h4 {
+    font-size: 0.8em;
+    transition: font-size 0.1s, bottom 0.1s;
+    bottom: 3rem;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { loadFonts } from './plugins/webfontloader'
+import './styles/index.css'
 
 loadFonts()
 
