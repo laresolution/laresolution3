bd8dd17edb0021312fb0d6baa54de11c7e8cd62e
Alexandre Vinet <alexandre.vinet@laresolution.ca>
2022-07-29 13:52:13 -0400

    fix unit test

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,21 +1,14 @@
 import { describe, it, expect } from "vitest";
-import { useVuetify } from '../../../vitest/vuetify'
 
 import { mount } from "@vue/test-utils";
 import HelloWorld from "../HelloWorld.vue";
 
 describe("HelloWorld", () => {
 
+  const wrapper = mount(HelloWorld)
 
   it("renders properly", async () => {
-    const wrapper = mount(HelloWorld, {
-      global: {
-        plugins: [useVuetify()]
-      }
-    });
-    // wrapper.get('[data-test="input"]').setValue('La Resolution')
-    
-    wrapper.get('[data-test="input"] input').setValue('La Resolution')
+    wrapper.get('[data-test="input"]').setValue('La Resolution')
     await wrapper.get('[data-test="button"]').trigger('click')
     expect(wrapper.get('[data-test="label"]').text()).toBe('La Resolution is a nice website!');
   });
