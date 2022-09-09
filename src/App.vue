bce19ee2d7a9f8b7f8a311ec35731683a83c3d21
Alexandre Vinet <alexandre.vinet@laresolution.ca>
2022-09-09 10:38:18 -0400

    add left menu example with routing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <div>
+      <ul>
+        <li>
+          <a href="/">Accueil</a>
+        </li>
+        <li>
+          <a href="new1">New 1</a>
+        </li>
+        <li>
+          <a href="new2">New 2</a>
+        </li>
+        <li>
+          <a href="new3">New 3</a>
+        </li>
+      </ul>
+    </div>
+    <div>
       <router-view/>
     </div>
   </div>
---
+++ src/views/NewView.vue
@@ -0,0 +1,24 @@
+<template>
+    This is my new view
+  </template>
+  
+  <script setup>
+  import { useForm } from 'vee-validate';
+  import RslvTextInput from '@/components/form/RslvTextInput.vue';
+  
+  // Define a validation schema
+  const simpleSchema = {
+    email(value) {
+      // validate email value and return messages...
+    },
+    password(value) {
+      // validate password value and return messages...
+    },
+  };
+  
+  // Create a form context with the validation schema
+  useForm({
+    validationSchema: simpleSchema,
+  });
+  </script>
+  
